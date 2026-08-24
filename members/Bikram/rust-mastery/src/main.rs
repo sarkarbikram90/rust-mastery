8b885b0afa543e7993d8e1fb6b338f198fd6e019
Bikram Sarkar <sarkarbikram90@gmail.com>
2026-08-24 14:23:11 -0500

    day1 of rust mastery

---
+++ members/Bikram/rust-mastery/src/main.rs
@@ -0,0 +1,23 @@
+// Rust programming language
+// Anatomy of a rust programming language
+// fn is the keyword to define a function in rust
+// println! is a macro in rust
+// let is the keyword to define an immutable variable in rust
+// {} is used as a placeholder for variables in rust
+// ; is used to end a statement in rust
+// rust is a compiled language
+// cargo run compiles and runs the program
+// cargo doc generates documentation for the program
+
+
+fn main() {
+    println!("Hello, world!");
+ // let is for immutable variable, meaning the varibale value cannot be changed later   
+
+    let day = "Monday";
+    println!("Day: {}", day);
+
+    let num = 2026;
+    println!("Number: {}", num);
+    
+}
