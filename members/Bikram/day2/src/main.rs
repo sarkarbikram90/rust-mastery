ce2deee7a4786f3759fa393d9a61e062cab6bacd
Bikram Sarkar <sarkarbikram90@gmail.com>
2026-08-25 15:08:51 -0500

    add:day2

---
+++ members/Bikram/day2/src/main.rs
@@ -0,0 +1,24 @@
+/*
+fn main() {
+    let x = 5; // assign value 5
+    println!("The value of x is: {x}");
+    x = 6; // changing value to 6
+    println!("The value of x is: {x}");
+}
+*/
+
+// The above code will throw an error 
+// as var value can't be changed
+// To change the value of a variable, we need to use the 'mut' keyword
+// 'mut' is short for mutable
+
+fn main() {
+    let mut x = 5; // assign value 5
+    println!("The value of x is: {x}");
+    x = 6; // changing value to 6
+    println!("The value of x is: {x}");
+}
+
+
+
+
