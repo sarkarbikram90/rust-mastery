440896e64959eecf907115eec5c6c835cf5139b5
Bikram Sarkar <sarkarbikram90@gmail.com>
2026-08-31 15:23:03 -0500

    add:day6

--- members/Bikram/day6/src/main.rs
+++ members/Bikram/day6/src/main.rs
@@ -10,6 +10,7 @@ fn main() {
 
 fn calculate_length(s: &String) -> usize { // s is a reference to a String
     s.len()
-} // Here, s goes out of scope. But because s does not have ownership of what
-  // it refers to, the String is not dropped.
+} 
+// Here, s goes out of scope. But because s does not have ownership of what
+// it refers to, the String is not dropped.
 
