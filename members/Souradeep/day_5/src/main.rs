3839f25cc7d77acf7af3a3f9ca2aa31759614569
Souradeep Ghosh <ghoshsouradeep7@gmail.com>
2026-09-08 00:27:26 +0530

    add:day5

--- members/Souradeep/day_5/src/main.rs
+++ members/Souradeep/day_5/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    // let s1 = String::from("hello");
+    // let s2 = s1; // s1 is moved to s2, s1 is no longer valid
+
+    // println!("{s1}, world!"); // this will cause a compile-time error because s1 is no longer valid
+    
+    // scope and assignment
+    let mut s = String::from("hello");
+    s = String::from("ahoy");
+
+    println!("{s}, world!");
+
+    // clone function 
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    let len = calculate_length(&s1);
+    println!("The length of '{s1}' is {len}.");
+
+    println!("s1 = {s1}, s2 = {s2}");
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
