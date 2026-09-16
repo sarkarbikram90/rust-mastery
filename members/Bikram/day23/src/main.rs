9669af45a7b83e3f7f8413cbe5ff9c53d150be17
sarkarbikram90 <sarkarbikram90@gmail.com>
2026-09-16 15:02:11 -0500

    add:day23

---
+++ members/Bikram/day23/src/main.rs
@@ -0,0 +1,45 @@
+// Packages, Crates, and Modules
+// By grouping related functionality 
+// and separating code with distinct features, 
+// clarify where to find code that implements a particular feature and where to go to change how a feature works.
+// Rust has three concepts for organizing code:
+// Packages
+// Crates
+// Modules
+
+// organize code by splitting it 
+// into multiple modules and then multiple files. 
+// A package can contain multiple 
+// binary crates and optionally one library crate. 
+// As a package grows, you can 
+// extract parts into separate crates 
+// that become external dependencies.
+
+// Rust has a number of features that 
+// allow you to manage your 
+// code’s organization, 
+// including which 
+// details are exposed, 
+// which details are private, and 
+// what names are in each 
+// scope in your programs. 
+// These features, sometimes collectively 
+// referred to as the 
+// module system, include:
+
+// Packages: A Cargo feature that 
+// lets you build, test, and share crates
+
+// Crates: A tree of modules that 
+// produces a library or executable
+
+// Modules and use: Let you control the 
+// organization, scope, and 
+// privacy of paths
+
+// Paths: A way of naming an item, 
+// such as a struct, function, or module
+
+// https://doc.rust-lang.org/book/ch07-02-defining-modules-to-control-scope-and-privacy.html
+// will continue tomorrow, exhausted after ofice work.
+
