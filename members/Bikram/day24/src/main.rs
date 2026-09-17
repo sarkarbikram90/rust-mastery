2cb634771858f6b27f51569cbef65598d14f9245
sarkarbikram90 <sarkarbikram90@gmail.com>
2026-09-17 14:16:50 -0500

    add:day24

---
+++ members/Bikram/day24/src/main.rs
@@ -0,0 +1,31 @@
+// How to Write Tests
+// Tests are Rust functions that 
+// verify that the non-test code is functioning 
+// in the expected manner. The bodies of test functions typically 
+// perform these three actions:
+
+// Set up any needed data or state.
+// Run the code you want to test.
+// Assert that the results are what you expect.
+
+// Structuring Test Functions
+// The file starts with an example add function so 
+// that we have something to test.
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+// The cargo test command runs all tests in our project
+// and displays output similar to this:
+
