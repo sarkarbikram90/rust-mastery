4e257164938981a2c50696c89febe093b97d00ef
Souradeep Ghosh <ghoshsouradeep7@gmail.com>
2026-09-24 00:48:35 +0530

    add:day12

--- members/Souradeep/day_12/src/main.rs
+++ members/Souradeep/day_12/src/main.rs
@@ -45,10 +45,15 @@ fn main() {
     let some_number = Some(5);
     let some_chars = Some('e');
 
-    let absent_number: Option<i32> = None;
+    // let absent_number: Option<i32> = None;
 
-    let x: i8 = 5;
-    let y: Option<i8> = Some(5);
+    // let x: i8 = 5;
+    // let y: Option<i8> = Some(5);
 
-    let sum = x + y
+    // let sum = match y {
+    //     Some(value) => x + value,
+    //     None => x,
+    // };
+
+    // println!("sum = {}", sum);
 }
